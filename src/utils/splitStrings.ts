ce0404368a5c4d3f1cfcb0fede73efb23c5aa293
cvalencia1991 <cesar4a6z@gmail.com>
2024-11-08 13:47:28 -0500

    Implement Split Strings to add the animations

---
+++ src/utils/splitStrings.ts
@@ -0,0 +1,11 @@
+export default function splitStringsUsingRegex(input: string): string[] {
+  const characters: string[] = [];
+  const regex = /[\s\S]/gu;
+
+  let match;
+  while ((match = regex.exec(input)) !== null) {
+    characters.push(match[0]);
+  }
+
+  return characters;
+}
